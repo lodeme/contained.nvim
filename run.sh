4b572b78416953dffbce64e9c9b9b6217e8d1326
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-04-09 01:51:24 +0200

    improved run script to avoid reinitializing container

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker run -it -v $HOME:/home nvim-container fish
+# Name of the Docker container
+CONTAINER_NAME="nvim-container"
+
+# Check if the container already exists
+if docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+	echo "Container already exists. Starting container ${CONTAINER_NAME}..."
+	docker start -ai "${CONTAINER_NAME}"
+else
+	echo "Container does not exist. Creating and starting ${CONTAINER_NAME}..."
+	docker run -it --name "${CONTAINER_NAME}" -v $HOME:/home nvim-container fish
+fi
