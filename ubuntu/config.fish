4313282369744ba9a72dc7ae98699f383e9218fb
lodeme <lodemetz@student.42berlin.de>
2024-04-11 18:11:26 +0200

    fish configuration is now copied from a file

---
+++ ubuntu/config.fish
@@ -0,0 +1,8 @@
+neofetch
+alias v='nvim'
+alias lg='lazyygit'
+zoxide init fish | source
+function z; cd $(zoxide query $argv); end
+function zi; cd $(zoxide query -i $argv); end
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
