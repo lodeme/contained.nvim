e41d11c312bfd34f81fbc56d56a438512cd28d4a
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-04-12 00:47:25 +0200

    added alias to fish config

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,8 +1,16 @@
+# Get system information at startup
 neofetch
+
+# Set aliases for shortcuts
 alias v='nvim'
 alias lg='lazygit'
+alias gs='git fetch && git status'
+
+# Setup zoxide aliases
 zoxide init fish | source
 function z; cd $(zoxide query $argv); end
 function zi; cd $(zoxide query -i $argv); end
+
+# Set locale
 set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
