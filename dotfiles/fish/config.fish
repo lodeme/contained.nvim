84606655f608685ae36090bcb5352f83e1c466ff
louis-demetz <61187384+louis-demetz@users.noreply.github.com>
2024-04-14 14:16:47 +0200

    added full fish configuration with plugins

---
+++ dotfiles/fish/config.fish
@@ -0,0 +1,21 @@
+# Get system information at startup
+if status is-interactive
+	neofetch
+end
+
+# Set aliases for shortcuts
+alias v='nvim'
+alias lg='lazygit'
+alias gfs='git fetch && git status'
+alias gs='git status'
+alias fd='fdfind'
+alias fzd='cd $(fd --type directory | fzf)'
+
+# Setup zoxide aliases
+zoxide init fish | source
+function z; cd $(zoxide query $argv); end
+function zi; cd $(zoxide query -i $argv); end
+
+# Set locale
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
